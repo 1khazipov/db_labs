77eb233645a14ff5c84f926d69192cc67ccbee5a
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-03-29 19:21:39 +0300

    Update 1a.sql

--- week10/1a.sql
+++ week10/1a.sql
@@ -1,46 +1,63 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id INT,
-  name VARCHAR(50),
-  credit REAL,
-  currency VARCHAR(10)
-);
+CREATE TABLE accounts (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    credit float NOT NULL,
+    currency VARCHAR(255) NOT NULL);
 
-INSERT INTO accounts (id, name, credit, currency)
-VALUES (1, 'Account_1', 1000.00, 'RUB'),
-       (2, 'Account_2', 1000.00, 'RUB'),
-       (3, 'Account_3', 1000.00, 'RUB');
-     
+INSERT INTO accounts VALUES (1, 'Alice', 1000.00, 'RUB');
+INSERT INTO accounts VALUES (2, 'Bob', 1000.00, 'RUB');
+INSERT INTO accounts VALUES (3, 'Charlie', 1000.00, 'RUB');
+
+CREATE OR REPLACE FUNCTION send_money(id_from INT, id_to INT, amount float)
+returns BOOLEAN
+AS
+$$
+declare
+    balance float;
+BEGIN
+    SELECT credit into balance 
+    FROM accounts 
+    WHERE id = id_from;
+
+    if balance < amount THEN
+        return false;
+    END if;
+
+    UPDATE accounts 
+    SET credit = balance - amount 
+    WHERE id = id_from;
+
+    UPDATE accounts 
+    SET credit = balance + amount 
+    WHERE id = id_to;
+
+    return true;
+END;
+$$ language plpgsql;
+
 BEGIN;
-    SAVEPOINT sp1;
+    savepoint sp1;
 
-    UPDATE accounts 
-    SET credit = credit - 500.00
-    WHERE id = 1;
+    SELECT send_money(1, 3, 500);
 
-    UPDATE accounts 
-    SET credit = credit + 500.00
-    WHERE id = 3;
+    SELECT credit 
+    FROM accounts;
 
+    ROLLBACK to sp1;
 
-    SAVEPOINT sp2;
-    UPDATE accounts 
-    SET credit = credit - 700.00
-    WHERE id = 2;
+    savepoint sp2;
 
-    UPDATE accounts 
-    SET credit = credit + 700.00
-    WHERE id = 1;
+    SELECT send_money(2, 1, 700);
 
+    SELECT credit FROM accounts;
 
-    SAVEPOINT sp3;
-    UPDATE accounts 
-    SET credit = credit - 100.00
-    WHERE id = 2;
+    ROLLBACK to sp2;
 
-    UPDATE accounts 
-    SET credit = credit + 100.00
-    WHERE id = 3;
+    savepoint sp3;
+    
+    SELECT send_money(2, 3, 100);
+
+    SELECT credit FROM accounts;
+
+    ROLLBACK to sp3;
 COMMIT;
-
-SELECT id, credit 
-FROM accounts;
