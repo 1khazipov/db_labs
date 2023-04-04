8e291c1db7ff94107b570e2b0041155f73259edb
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-04 15:55:21 +0300

    Create ex1.py

---
+++ week11/ex1.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost")
+db = client['test']
+
+
+def select_irish():
+    cur = db.restaurants.find({'cuisine': 'Irish'})
+    for c in cur:
+        print(c)
+
+
+def select_russian_irish():
+    cur = db.restaurants.find({'cuisine': 'Russian'})
+    for c in cur:
+        print(c)
+    cur = db.restaurants.find({'cuisine': 'Irish'})
+    for c in cur:
+        print(c)
+
+
+def select_address():
+    cur = db.restaurants.find({'address.building': '284', 
+                               'address.street': 'Prospect Park West', 
+                               'address.zipcode': '11215'})
+    for c in cur:
+        print(c)
+
+
+select_irish()
+select_russian_irish()
+select_address()
